aa5658603c7a4ae10b8335d87fc19bfd7563852c
Erkan <erkan808987@gmail.com>
2020-06-12 22:22:24 -0400

    Added opengl parallelism

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,44 @@ use ggez::event::{self, EventHandler};
 use ggez::graphics;
 use ggez::graphics::{Canvas,Color,Drawable,Image};
 use ggez::input::mouse::MouseButton;
+use ocl::ProQue;
 use std::thread;
 use std::sync::Arc;
-
 const MAX_ITER:u32 = 256;
+
+const R_MIN:f32 = -2.25f32;
+const R_MAX:f32 = 0.75f32;
+const C_MIN:f32 = -1.5f32;
+const C_MAX:f32 = 1.5f32;
+
+static KERNEL_SRC: &'static str = r#"
+    __kernel void mandelbrot(__global unsigned int* iterations,
+                                 int width, int height,int iter_limit) {
+    const float r_max = -2.25f;
+    const float r_min = 0.75f;
+    const float c_max = 1.5f;
+    const float c_min = -1.5f;
+
+    int px = get_global_id(0);
+    int py = get_global_id(1);
+    if (px >= width || py >= height) return;
+
+    float x0 = r_min + px * ((r_max - r_min) / width);
+    float y0 = c_min + py * ((c_max - c_min) / height);
+    unsigned int iteration;
+    float x = 0.0f;
+    float y = 0.0f;
+    for (iteration = 0; iteration < iter_limit; iteration++) {
+        float xn = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xn;
+        if (x * x + y * y > 2.0f) {
+            break;
+        }
+    }
+    iterations[width * py + px] = iteration;
+    } 
+"#;
 
 struct App{
     canvas:Canvas,
@@ -46,8 +80,6 @@ fn build_grad() -> Gradient<LinSrgb>{
     
     let mut palette = Vec::new();
 
-    palette.push(LinSrgb::new(66.0,30.0,15.0));
-    palette.push(LinSrgb::new(25.0,7.0,26.0));
     palette.push(LinSrgb::new(9.0,1.0,47.0));
     palette.push(LinSrgb::new(4.0,4.0,47.0));
     palette.push(LinSrgb::new(0.0,7.0,100.0));
@@ -59,22 +91,29 @@ fn build_grad() -> Gradient<LinSrgb>{
     palette.push(LinSrgb::new(241.0,233.0,191.0));
     palette.push(LinSrgb::new(248.0,201.0,95.0));
     palette.push(LinSrgb::new(255.0,170.0,0.0));
-    palette.push(LinSrgb::new(204.0,128.0,0.0));
-    palette.push(LinSrgb::new(153.0,87.0,0.0));
     palette.push(LinSrgb::new(106.0,52.0,3.0));
-    palette.push(LinSrgb::new(0.0,0.0,0.0));
     Gradient::new(palette)
 }
 
 fn color(complex:Complex<f32>,iter:u32,buffer:&mut Rgba<u8>,grad:&Gradient<LinSrgb>){
-        let color = grad.get(iter as f32/MAX_ITER as f32).into_components();
-        buffer[0] = color.0 as u8;
-        buffer[1] = color.1 as u8;
-        buffer[2] = color.2 as u8;
-        buffer[3] = 255;
+            
+        if iter == MAX_ITER{
+            buffer[0] = 0;
+            buffer[1] = 0;
+            buffer[2] = 0;
+            buffer[3] = 255;
+        }else{
+            let x = (iter as f32/MAX_ITER as f32 * 32.0 + 1.0).log2();
+            let x = x/(32.0+1.0 as f32).log2();
+            let color = grad.get(x).into_components();
+            buffer[0] = color.0 as u8;
+            buffer[1] = color.1 as u8;
+            buffer[2] = color.2 as u8;
+            buffer[3] = 255;
+        }
 }
 
-fn generate_mendel(dim:u32) -> RgbaImage{
+fn generate_mendel_serial(dim:u32) -> RgbaImage{
     // Define img size, range of reals and i, and scale, iter range
     // It's found for 800x800, 256 iterations suffices
     let img_dim = dim;
@@ -110,10 +149,43 @@ fn generate_mendel(dim:u32) -> RgbaImage{
     return pixel_buffer;
 }
 
+/// OPEN-CL START
+
+
+    fn run(dims:(u32,u32)){
+
+
+        let pro_que = ProQue::builder()
+            .src(KERNEL_SRC)
+            .dims(dims)
+            .build().unwrap();
+
+        let buffer = pro_que.create_buffer::<u32>().unwrap();
+
+        let kernel = pro_que.kernel_builder("mandelbrot")
+            .arg(&buffer)
+            .arg(dims.0)
+            .arg(dims.1)
+            .arg(256u32)
+            .build().unwrap();
+
+        unsafe { kernel.enq().unwrap(); }
+        
+        let mut result = vec![0u32; buffer.len()];
+        buffer.read(&mut result).enq().unwrap();
+    
+        for iter in result.iter().filter(|&&x|x<256 && x>10){
+            println!("The value is: {}",iter);
+        }
+    }
+
+/// END
 fn main() {
     
+    run((800,800));
+    return;
     let mut data = RgbaImage::new(800,800);
-    data = generate_mendel(800);
+    data = generate_mendel_serial(800);
 
     let (mut ctx, mut event_loop) =
        ContextBuilder::new("Mandelbrot Set", "Erkan")
